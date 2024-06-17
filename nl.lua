c3f96ea095a15a0ab07cfb883b582104a728d085
SnowpixelMedia <69346166+in2gamingnl@users.noreply.github.com>
2024-06-17 12:03:13 +0200

    nl.lua
    Dutch locale

---
+++ nl.lua
@@ -0,0 +1,103 @@
+Language['en'] = {
+    keybind_desc = 'Open crew menu',
+    toggle_tags = 'Crew tags zijn nu %s!',
+    tags_enabled = 'zichtbaar',
+    tags_disabled = 'verborgen',
+
+    -- MAIN MENU
+    main_menu_title = 'CREW MENU',
+    main_menu_desc = 'Tag: [%s]\n Leden: %s\n Je rang: %s',
+    main_menu_invite_title = 'SPELERS UITNODIGEN',
+    main_menu_invite_desc = 'Klik om spelers uit te nodigen.',
+    main_menu_manage_title = 'SPELERS BEHEREN',
+    main_menu_manage_desc = 'Klik om het spelersbeheer te openen.',
+    main_menu_delete_title = 'VERWIJDER CREW',
+    main_menu_delete_desc = 'Klik om de crew te verwijderen.',
+    main_menu_settings_title = 'INSTELLINGEN',
+    main_menu_settings_desc = 'Klik om de crew instellingen te openen.',
+    main_menu_leave_title = 'VERLATEN',
+    main_menu_leave_desc = 'Klik om de crew te verlaten.',
+    main_menu_invites_title = 'TOON UITNODIGINGEN',
+    main_menu_invites_desc = 'Klik om je uitnodigingen weer te geven.',
+
+    -- CREATING A CREW
+    main_menu_create_title = 'CREW AANMAKEN',
+    main_menu_create_desc = 'Klik om een crew aan te maken.',
+    create_name_desc = 'Naam invoeren...',
+    create_tag_desc = 'Tag invoeren. (4 chars.)',
+    create_success = 'Succesvol gemaakt "%s"!',
+
+    -- CREW DELETION
+    delete_confirmation_title = 'VERWIJDER CREW',
+    delete_confirmation_desc = 'Weet je zeker dat je deze crew wilt verwijderen?',
+    delete_success = 'Met succes verwijderd "%s".',
+
+    -- LEAVING CREW
+    leave_confirmation_title = 'CREW > VERLATEN',
+    leave_confirmation_desc = 'Weet je zeker dat je je crew bemanning wilt verlaten?',
+    leave_success = 'Je hebt de crew met succes verlaten.',
+
+    -- INVITES
+    invites_title = 'CREW > UITNODINGEN',
+    invites_btn_desc = 'Klik om te accepteren.',
+    invites_received_new = 'Je hebt een uitnodiging voor een crew ontvangen!',
+    invites_success = 'U bent lid geworden van "%s".',
+
+    -- MEMBER LIST
+    member_list_title = 'CREW > LEDEN',
+    member_list_owner_title = '%s (Jij)',
+
+    -- MANAGING MEMBERS
+    manage_title = 'CREW > BEHEER',
+    manage_btn_desc = 'Klik om deze speler te beheren.',
+    manage_no_players = 'Er is niemand om te managen.',
+
+    -- MEMBER SETTINGS
+    member_title = 'LEDEN > %s',
+    member_rank_title = 'RANG WIJZIGEN',
+    member_rank_desc = 'Rang: %s',
+    member_rank_input_title = 'Selecteer een rang',
+    member_rank_confirmation_title = 'RANG WIJZIGEN',
+    member_rank_confirmation_desc = 'Weet je zeker dat je de rang van %s wilt wijzigen naar %s?',
+    member_rank_success = 'Rang van %s succesvol gewijzigd naar "%s"!',
+    player_rank_changed = 'Je rang is gewijzigd naar "%s"!',
+    member_crew_transfer_title = 'OVERDRACHT EIGENDOM',
+    member_crew_transfer_desc = 'Klik om eigendom over te dragen.',
+    member_crew_transfer_confirmation_desc = 'Weet je zeker dat je het eigendom van de crew wilt overdragen aan %s?',
+    member_crew_transfer_success = 'Crew succesvol overgedragen aan "%s"!',
+    player_crew_transfered = 'Het eigendom van de crew is aan jou overgedragen.',
+    member_kick_title = 'UITZETTEN',
+    member_kick_desc = 'Klik om deze speler uit te zetten.',
+    member_kick_confirmation_title = 'SPELER UITZETTEN',
+    member_kick_confirmation_desc = 'Weet je zeker dat je "%s" wilt uitzetten?',
+    member_kick_success = 'Speler succesvol uitgezet!',
+    player_kicked = 'Je bent uit je crew gezet!',
+    
+    member_rank_owner = 'Leider',
+    member_rank_member = 'Lid',
+    
+    -- SPELERS UITNODIGEN
+    invite_title = 'CREW > UITNODIGEN',
+    invite_btn_desc = 'Klik om uit te nodigen.',
+    invite_success = 'Je hebt "%s" uitgenodigd voor de crew!',
+    
+    -- INSTELLINGEN
+    settings_title = 'CREW > INSTELLINGEN',
+    settings_btn_rename_title = 'HERNOEMEN',
+    settings_btn_rename_desc = 'Klik om de naam van de crew te wijzigen.',
+    settings_btn_tag_title = 'TAG WIJZIGEN',
+    settings_btn_rename_desc = 'Klik om de tag van de crew te wijzigen.',
+    rename_success = 'Je crew is hernoemd naar "%s Crew"',
+    tag_success = 'Je crew tag is gewijzigd naar "%s"',
+    
+    -- ERRORS
+    error_name_used = 'Deze naam is al in gebruik.',
+    error_tag_used = 'Deze tag is al in gebruik.',
+    error_tag_invalid = 'Tag moet 4 tekens lang zijn.',
+    error_no_invites = 'Je hebt geen uitnodigingen!',
+    error_nobody_around = 'Er zijn geen spelers in de buurt!',
+    error_already_invited = 'Je hebt deze speler al uitgenodigd.',
+    error_player_in_crew = 'Deze speler is al onderdeel van een andere crew.',
+    error_limit_reached = 'Je hebt het spelerslimiet bereikt! (%s)',
+    error_no_crew = 'Je hebt geen crew!',
+}
